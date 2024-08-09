1bd77d221b9b4c0c9e3795d99e32a3bd431c0f6a
Elyaaa001 <gabibovelina75@gmail.com>
2024-08-09 13:02:24 +0400

    fix

--- sort.h
+++ sort.h
@@ -30,6 +30,4 @@ int lomuto_partition(int *array, int low, int high, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
 
-#endif 
-~                                                                                          
-~       
+#endif   
